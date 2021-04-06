4b4b09f622e6a9ed4cbbaaf3f09c726ab0002ea0
Hogar23 <petar.lazarevic23@gmail.com [core]>
2021-04-06 11:11:58 +0200

    added reportUrl to payments

--- src/DigiWallet.js
+++ src/DigiWallet.js
@@ -17,6 +17,7 @@ export default class DigiWallet {
 
   sofort() {
     const sofort = new Sofort();
+    sofort.reportUrl = this.reportUrl;
     sofort.returnUrl = this.returnUrl;
     sofort.cancelUrl = this.cancelUrl;
     sofort.appId = this.appId;
@@ -27,6 +28,7 @@ export default class DigiWallet {
 
   paysafecard() {
     const paysafecard = new Paysafecard();
+    paysafecard.reportUrl = this.reportUrl;
     paysafecard.returnUrl = this.returnUrl;
     paysafecard.cancelUrl = this.cancelUrl;
     paysafecard.test = this.test;
@@ -37,6 +39,7 @@ export default class DigiWallet {
 
   afterpay() {
     const afterpay = new Afterpay();
+    afterpay.reportUrl = this.reportUrl;
     afterpay.returnUrl = this.returnUrl;
     afterpay.cancelUrl = this.cancelUrl;
     afterpay.test = this.test;
@@ -47,6 +50,7 @@ export default class DigiWallet {
 
   bancontact() {
     const bancontact = new Bancontact();
+    bancontact.reportUrl = this.reportUrl;
     bancontact.returnUrl = this.returnUrl;
     bancontact.cancelUrl = this.cancelUrl;
     bancontact.test = this.test;
@@ -57,6 +61,7 @@ export default class DigiWallet {
 
   creditcard() {
     const creditcard = new Creditcard();
+    creditcard.reportUrl = this.reportUrl;
     creditcard.returnUrl = this.returnUrl;
     creditcard.cancelUrl = this.cancelUrl;
     creditcard.test = this.test;
@@ -68,6 +73,7 @@ export default class DigiWallet {
 
   ideal() {
     const ideal = new Ideal();
+    ideal.reportUrl = this.reportUrl;
     ideal.returnUrl = this.returnUrl;
     ideal.cancelUrl = this.cancelUrl;
     ideal.test = this.test;
@@ -78,6 +84,7 @@ export default class DigiWallet {
 
   paypal() {
     const paypal = new Paypal();
+    paypal.reportUrl = this.reportUrl;
     paypal.returnUrl = this.returnUrl;
     paypal.cancelUrl = this.cancelUrl;
     paypal.test = this.test;
